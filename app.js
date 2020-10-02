ad3b01a49ed528978444ee92f240ef498324b1aa
DavidSharp <david.sharp@nationwide.co.uk>
2020-10-02 10:11:13 +0100

    feat: amended package.json to start with nodemon

--- app.js
+++ app.js
@@ -4,5 +4,5 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 app.listen(3000, function () {
-  console.log('This is our first app');
+  console.log('our first app is up and running 1337!');
 });
